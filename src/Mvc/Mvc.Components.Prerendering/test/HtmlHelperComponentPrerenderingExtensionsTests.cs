11bd2d885e25220c6c2a66390b87456ced71f4f1
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-04-02 12:58:37 -0700

    Update namespace for HtmlHelperRazorComponentExtensions

--- src/Mvc/Mvc.Components.Prerendering/test/HtmlHelperComponentPrerenderingExtensionsTests.cs
+++ src/Mvc/Mvc.Components.Prerendering/test/HtmlHelperComponentPrerenderingExtensionsTests.cs
@@ -28,7 +28,7 @@ namespace Microsoft.AspNetCore.Mvc.ViewFeatures
             var helper = CreateHelper(null, s => { });
             var writer = new StringWriter();
             var expectedmessage = $"No 'IComponentPrerenderer' implementation has been registered in the dependency injection container. " +
-                    $"This typically means a call to 'services.AddRazorComponents()' is missing in 'Startup.ConfigureServices'.";
+                    $"This typically means a call to 'services.AddServerSideBlazor()' is missing in 'Startup.ConfigureServices'.";
 
             // Act & Assert
             var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => helper.RenderComponentAsync<TestComponent>());
